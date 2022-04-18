bc49a8b59a687dab3878418d3e43fe6cf45e7cfc
NguyenThanhNhan2507 <nhandaica125@gmail.com>
2022-04-18 23:03:14 +0700

    update frontend

---
+++ .history/frontend/src/funtions/pageBookingMovie/pageBookingMovie_20220418225906.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import NavBar from '../../components/navbar/navBar';
+
+
+
+function PageBookingMovie() {
+
+    return (
+        <NavBar/>
+    );
+}
+
+export default PageBookingMovie;
---
+++ frontend/src/funtions/pageBookingMovie/pageBookingMovie.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import NavBar from '../../components/navbar/navbar';
+
+
+
+function PageBookingMovie() {
+
+    return (
+        <NavBar/>
+    );
+}
+
+export default PageBookingMovie;
