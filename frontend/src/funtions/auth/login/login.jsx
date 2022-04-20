da215e1517c20479b5d19803db0d324ee4fb244c
NguyenThanhNhan2507 <nhandaica125@gmail.com>
2022-04-20 12:08:23 +0700

    update login frontend

---
+++ frontend/src/funtions/auth/login/login.jsx
@@ -0,0 +1,98 @@
+import { Button, IconButton } from "@material-ui/core";
+import React from "react";
+import { TextValidator, ValidatorForm } from "react-material-ui-form-validator";
+import {  } from "react-redux";
+import { Link,  } from "react-router-dom";
+import {  ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import logo from "../../../assets/images/logocam.png";
+import "./login.css"
+
+function LogIn() {
+
+  return (
+    <div className="page">
+      <ToastContainer />
+      <div className="backgroundBlur"></div>
+      <div className="container">
+        <section className="fullnavbar">
+          <nav className="navbar navbar-expand-sm">
+            <Link className="navbar-brand" to="/">
+              <img className="logoWeb" src={logo} alt="logo" />
+            </Link>
+          </nav>
+        </section>
+        <section className="loginContent">
+          <div className="fullLogin">
+            <div className="loginContent--form">
+              <h1 data-uia="login-page-title">Đăng nhập</h1>
+              <ValidatorForm className="login--form__email"
+              >
+                <TextValidator
+                  className="login--form__input"
+                  id="id_userLoginId"
+                  defaultValue
+                  placeholder="Nhập email"
+                  variant="outlined"
+                  margin="normal"
+                  fullWidth
+                  name="email"
+                  validators={["required", "isEmail"]}
+                  errorMessages={[
+                    "Hãy điền trường này!!!",
+                    "email không hợp lệ",
+                  ]}
+                  autoComplete="on"
+                />
+                <div className="passwordFull">
+                  <TextValidator
+                    className="login--form__input"
+                    id="id_password"
+                    defaultValue
+                    placeholder="Nhập password"
+                    variant="outlined"
+                    fullWidth
+                    name="password"
+                    validators={["required"]}
+                    errorMessages={["Hãy điền trường này!!!"]}
+                    autoComplete="on"
+                  ></TextValidator>
+                  <IconButton
+                    className="showPassword"
+                    aria-label="toggle password visibility"
+                  >
+                  </IconButton>
+                </div>
+
+                <div className="login--form__button">
+                  <Button
+                    className="signinHome"
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                  >
+                    Đăng nhập
+                  </Button>
+                </div>
+              </ValidatorForm>
+            </div>
+            <div className="loginContent--other">
+              <div className="login-signup-now" data-uia="login-signup-now">
+                Bạn mới tham gia ?
+                <Link to="/">
+                  <a className=" " target="_self">
+                    {" "}
+                    Đăng ký ngay
+                  </a>
+                  .
+                </Link>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default LogIn;
